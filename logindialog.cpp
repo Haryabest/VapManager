cdbad2898c0c63146da05ded6ee6af744601e356
unknown <delfinchik.v05@gmail.com>
2026-04-06 11:35:39 +0300

    рус текствв

--- logindialog.cpp
+++ logindialog.cpp
@@ -170,7 +170,7 @@ LoginDialog::LoginDialog(QWidget *parent)
     btnLogin->setMinimumHeight(42);
     v->addWidget(btnLogin);
 
-    QPushButton *btnReg = new QPushButton("REg", loginPage);
+    QPushButton *btnReg = new QPushButton("Регистрация", loginPage);
     btnReg->setObjectName("secondaryBtn");
     btnReg->setMinimumHeight(40);
     v->addWidget(btnReg);
